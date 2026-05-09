1fdb6b3981c8ab74bb4228a8ea2742f5d129fe98
Juan Francisco <neofenixlive@gmail.com>
2026-05-09 17:30:40 -0300

    Update pilha_is.c

--- pilha_is.c
+++ pilha_is.c
@@ -1,8 +1,8 @@
 #include "pilha.h"
 
 void __push(pilha *p, i32 value)          { pilha_push(p, value); }
-void __pushat(pilha *p, i32 value)        { pilha_push(p, p->vvariable[value]); }
 void __pop(pilha *p, i32 value)           { pilha_pop(p); }
+void __pushat(pilha *p, i32 value)        { pilha_push(p, p->vvariable[value]); }
 void __popat(pilha *p, i32 value)         { p->vvariable[value] = pilha_pop(p); }
 void __copy(pilha *p, i32 value)          { i32 a = pilha_pop(p); pilha_push(p, a); pilha_push(p, a); }
 void __swap(pilha *p, i32 value)          { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, a); pilha_push(p, b); }
@@ -18,16 +18,16 @@ void __maximum(pilha *p, i32 value)       { i32 a = pilha_pop(p), b = pilha_pop(
 void __minimum(pilha *p, i32 value)       { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, (b < a) ? b : a); }
 void __equal(pilha *p, i32 value)         { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b == a); }
 void __more(pilha *p, i32 value)          { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b > a); }
-void __moreequal(pilha *p, i32 value)     { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b >= a); }
 void __less(pilha *p, i32 value)          { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b < a); }
+void __moreequal(pilha *p, i32 value)     { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b >= a); }
 void __lessequal(pilha *p, i32 value)     { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b <= a); }
 void __and(pilha *p, i32 value)           { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b && a); }
 void __or(pilha *p, i32 value)            { i32 a = pilha_pop(p), b = pilha_pop(p); pilha_push(p, b || a); }
 void __not(pilha *p, i32 value)           { pilha_push(p, !pilha_pop(p)); }
 void __jump(pilha *p, i32 value)          { p->bytecode_index = p->vvariable[value]; }
+void __exit(pilha *p, i32 value)          { p->bytecode_index = p->bytecode_size; }
 void __iftrue(pilha *p, i32 value)        { if(!pilha_pop(p)) p->bytecode_index++; }
 void __iffalse(pilha *p, i32 value)       { if(pilha_pop(p)) p->bytecode_index++; }
-void __exit(pilha *p, i32 value)          { p->bytecode_index = p->bytecode_size; }
 
 void __call(pilha *p, i32 value) {
     p->cstack[p->cstack_top] = p->bytecode_index;
@@ -74,9 +74,9 @@ pilha_token pilha_mnemonic[] = {
     {"OR",        __or},
     {"NOT",       __not},
     {"JUMP",      __jump},
+    {"EXIT",      __exit},
     {"IFTRUE",    __iftrue},
     {"IFFALSE",   __iffalse},
-    {"EXIT",      __exit},
     {"CALL",      __call},
     {"RETURN",    __return},
     {NULL,        NULL}
